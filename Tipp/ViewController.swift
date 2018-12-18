ddd23adc484eeb355233dbcd4465025e7013943c
Honors <honors@honorslapt-b59853.fios-router.home>
2018-12-17 21:48:11 -0500

    Initial Commit

---
+++ Tipp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipp
+//
+//  Created by Honors on 12/17/18.
+//  Copyright © 2018 Ruchi Vaidya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
